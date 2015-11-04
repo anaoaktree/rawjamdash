b5c879c6ce9fe8a0fb092454abc62e4f3781c05a
anapmc.carvalho@gmail.com <anapmc.carvalho@gmail.com>
2015-11-04 11:13:36 +0000

    today time

--- hello/views.py
+++ hello/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import datetime
+
+
 from .models import Greeting
 
 # Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, 'index.html', {'todaytime': "it worked!"})
+    now = datetime.datetime.now()
+    return render(request, 'index.html', {'todaytime': now})
 
 
 def db(request):
