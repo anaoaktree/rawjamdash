7378f1a8a84b5a3f584b89b4f0a80b2b94d35a1a
anapmc.carvalho@gmail.com <anapmc.carvalho@gmail.com>
2015-11-04 19:13:14 +0000

    db

--- hello/models.py
+++ hello/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 
 class Person(models.Model):
-	firstName = models.CharField(max_length=30,db_tablespace="indexes")
+	name = models.CharField(max_length=30,db_tablespace="indexes")
 	#last_name = models.CharField(max_length=30) #release_date = models.DateField()
 	#age = models.IntegerField()
 	#bio = models.CharField(max_length=30)
