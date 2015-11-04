01c1f3ff96be26e951d5a304fba72bf293703def
anapmc.carvalho@gmail.com <anapmc.carvalho@gmail.com>
2015-11-04 13:19:09 +0000

    try db

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -12,5 +12,7 @@ urlpatterns = patterns('',
      url(r'^faker/', include('django_faker.urls')),
     url(r'^$', hello.views.index, name='index'),
     url(r'^tables/', hello.views.tables, name='tables'),
+    url(r'^db/', hello.views.db, name='db'),
+
     url(r'^admin/', include(admin.site.urls)),
 )
