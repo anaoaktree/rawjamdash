9cb6034466815d3c32b315c2f7f229488af322be
anapmc.carvalho@gmail.com <anapmc.carvalho@gmail.com>
2015-11-04 14:51:46 +0000

    big table try spinner

---
+++ hello/static/hello/js/dataTables/bigTableAjax.js
@@ -0,0 +1,39 @@
+function createRequest() {
+  try {
+    request = new XMLHttpRequest();
+  } catch (trymicrosoft) {
+    try {
+      request = new ActiveXObject ("Msxml2.XMLHTTP");
+  } catch (othermicrosoft) {
+    try {
+      request = new ActiveXObject("Microsoft.XMLHTTP");
+    } catch (failed) {
+      request = null;
+    }
+  }
+}
+
+if (request == null)
+  alert("Error creating request object!");
+return request;
+};
+
+
+function getBigTable(){
+  //ajax request to get conditions
+  event.preventDefault();
+  xhr = createRequest();
+  xhr.open("GET", "/bigTable/", true);
+  xhr.onreadystatechange = function(e) {
+  if (xhr.readyState == 4) {
+        var spinner= document.getElementById('spinner');
+        spinner.style.visibility = "hidden";
+
+      }
+  }
+}
+
+window.onload = function(){
+  //checked jsperf.com for perfomance
+ getBigTable()
+};
