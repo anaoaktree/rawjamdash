fed70096534e0233dab5d85a468cc8a5b18f7dd1
anapmc.carvalho@gmail.com <anapmc.carvalho@gmail.com>
2015-11-04 16:47:18 +0000

    no greetings

--- hello/models.py
+++ hello/models.py
@@ -2,9 +2,6 @@ from django.db import models
 
 
 # Create your models here.
-class Greeting(models.Model):
-    when = models.CharField()
-
 
 class Person(models.Model):
 	firstName = models.CharField(max_length=30)
