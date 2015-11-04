453db8f2c80d6d0bb15c8d4cab18e1a84af8c0dc
anapmc.carvalho@gmail.com <anapmc.carvalho@gmail.com>
2015-11-04 10:38:39 +0000

    bug push

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -9,7 +9,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'gettingstarted.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
     url(r'^$', hello.views.index, name='index'),
     url(r'^db', hello.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
