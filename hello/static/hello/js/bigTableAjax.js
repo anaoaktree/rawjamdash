eba456e60a6397e60b933f9926185c2e2c3e82e2
anapmc.carvalho@gmail.com <anapmc.carvalho@gmail.com>
2015-11-05 03:43:13 +0000

    try drag

---
+++ hello/static/hello/js/bigTableAjax.js
@@ -0,0 +1,43 @@
+function createRequest() {
+  try {
+    request = new XMLHttpRequest();
+  } catch (trymicrosoft) {
+    try {
+      request = new ActiveXObject ("Msxml2.XMLHTTP");
+  } catch (othermicrosoft) {
+    try {
+      request = new ActiveXObject("Microsoft.XMLHTTP");
+    } catch (failed) {
+      request = null;
+    }
+  }
+}
+
+if (request == null)
+  alert("Error creating request object!");
+return request;
+};
+
+
+function populateTable(){
+  xhr = createRequest();
+  xhr.onreadystatechange = function(e) {
+    if (xhr.readyState == 4) {
+    htmldoc = $($.parseHTML(xhr.responseText));
+    document.getElementById("bigTable").innerHTML = htmldoc.find("#bigTable").html(); 
+    $('#dataTables-example').dataTable()
+        }
+      }
+    xhr.open("GET", "/bigTable/", true);
+    xhr.send()
+
+}
+
+
+
+  $(document).ready(function () {
+      $('#ageTable').dataTable();
+      $('#timeTable').dataTable();
+      populateTable()
+     
+    });
