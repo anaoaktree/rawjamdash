c4ee2f08dedb3f990843309679ffa1cc03d182b5
anapmc.carvalho@gmail.com <anapmc.carvalho@gmail.com>
2015-11-05 00:08:13 +0000

    charts and dashboard

--- hello/static/hello/js/charts.js
+++ hello/static/hello/js/charts.js
@@ -12,10 +12,14 @@ c3.generate({
             ['51-59', 86],
             ['>60', 100],
         ],
-        type : 'donut',
+        type : 'bar',
     },
-    donut: {
-        title: "Age distribution"
+    bar: {
+        width: {
+            ratio: 0.5 // this makes bar width 50% of length between ticks
+        }
+        // or
+        //width: 100 // this makes bar width 100px
     }
 });
 
@@ -31,7 +35,10 @@ c3.generate({
 
 
         ],
-        type : 'pie',
+        type : 'donut',
+    },
+    donut: {
+        title: "Traffic distribution"
     }
 });
     var genderchart = c3.generate({
