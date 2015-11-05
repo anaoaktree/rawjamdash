4c3af24fbb38893f75005f5b1a6249505bf2012d
anapmc.carvalho@gmail.com <anapmc.carvalho@gmail.com>
2015-11-05 01:25:47 +0000

    try django admin

--- hello/admin.py
+++ hello/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from django.contrib import admin
+from hello.models import SubUser
+
+class SubUserAdmin(admin.ModelAdmin):
+    pass
+
+
+
+
+
+admin.site.register(SubUser, SubUserAdmin)
--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 import datetime,json, random
 from hello.models import SubUser
