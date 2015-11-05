ee0d1f7063345d84a08b177b1a3236424bcd4beb
anapmc.carvalho@gmail.com <anapmc.carvalho@gmail.com>
2015-11-05 04:58:43 +0000

    wrap up

--- hello/models.py
+++ hello/models.py
@@ -7,6 +7,8 @@ class Person(models.Model):
 	firstName = models.CharField(max_length=30,db_tablespace="indexes")
 	
 
+
+#Model of the subway user : his name, gender, age, when he last traveled
 class SubUser(models.Model):
 	GEN = (("M","Male"),("F", "Female"))
 	name = models.CharField(max_length=30,db_tablespace="indexes")
